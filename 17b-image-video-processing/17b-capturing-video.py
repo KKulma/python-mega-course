5929cded239bad029ebd4f0b00a6b2cfd563c15e
Kasia Kulma <kasia.kulma@effem.com>
2022-02-02 21:52:24 +0000

    improve the video script

--- 17b-image-video-processing/17b-capturing-video.py
+++ 17b-image-video-processing/17b-capturing-video.py
@@ -2,19 +2,39 @@ import cv2
 import time
 
 # record a video: trigger the webcam, keep it on for 3 secs and then stop
-video = cv2.VideoCapture()
+video = cv2.VideoCapture(0)
 
-## commented out bits not working as expected so I skipped the remaining code for recording the video live
-check, frame = video.read()
-# print(check)
-# print(frame)
+a = 1
+while True:
+    a = a+1
+    check, frame = video.read()
+    print(check)
+    print(frame)
 
-time.sleep(2)
-cv2.imshow("Capturing", frame)
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BayerGR2GRAY)
 
-cv2.waitKey(2000)
+    cv2.imshow("Capturing", frame)
+    key = cv2.waitKey(1)
+
+    if key==ord('q'):
+        break
+
+print(a)
 video.release()
 cv2.destroyAllWindows()
 
 
-# video = cv2.VideoCapture('video.mov')
+# ### code tried earlier
+#
+# video = cv2.VideoCapture(0)
+#
+# check, frame = video.read()
+# # print(check)
+# # print(frame)
+#
+# time.sleep(2)
+# cv2.imshow("Capturing", frame)
+#
+# cv2.waitKey(2000)
+# video.release()
+# cv2.destroyAllWindows()
