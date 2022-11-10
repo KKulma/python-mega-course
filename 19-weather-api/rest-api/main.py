31a329cee616bfe9917bddc55f8ced50aa76ac60
Kasia Kulma <katarzyna.kulma@gmail.com>
2022-11-10 23:14:17 +0000

    19-serve real data in REST API

--- 19-weather-api/rest-api/main.py
+++ 19-weather-api/rest-api/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,7 +9,11 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
-    temperature=23
+    filename = f'data_small/TG_STAID{str(station).zfill(6)}.txt'
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    temperature = df[df['    DATE'] == date]['   TG'].squeeze()/10
+    print(temperature)
+
     return {
         "station": station,
         "date": date,
